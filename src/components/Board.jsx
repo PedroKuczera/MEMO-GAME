fef8a09c21d7ba89335baa879482be3850f094a6
Pedro Kuczera <pedrohkd001@gmail.com>
2025-08-26 17:46:14 -0300

    renderizando listas

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,12 +1,14 @@
 import React from "react"
 import { Card } from "./Card"
 
+const icons = [
+    '🎶', '👍', '😎', '🤩',
+    '😖', '😈', '🤓', '🫣'
+]
 export function Board() {
     return (
         <div style={style}>
-            <Card>😁</Card>
-            <Card>😆</Card>
-            <Card>😅</Card>
+            {[...icons, ...icons].map((icon, index) => (<Card key={index}>{icon}</Card>))}
         </div>
     )
 }
@@ -15,8 +17,8 @@ const style = {
     backgroundColor: '#464646ff',
     flexGrow: 1,
     display: 'grid',
-    gridTemplateColumns: 'repeat(5, 1fr)',
-    gridTemplateRows: 'repeat(5, 1fr)',
+    gridTemplateColumns: 'repeat(4, 1fr)',
+    gridTemplateRows: 'repeat(4, 1fr)',
     gap: '1em',
     padding: '1em'
 }
