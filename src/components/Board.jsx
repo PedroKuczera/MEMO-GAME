52298154c956a60d9a9833ea09430067b5ca1dee
Pedro Kuczera <pedrohkd001@gmail.com>
2025-08-26 14:45:58 -0300

    Estilos CSS e Props dos componentes

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -3,10 +3,20 @@ import { Card } from "./Card"
 
 export function Board() {
     return (
-        <div>
-            <Card />
-            <Card />
-            <Card />
+        <div style={style}>
+            <Card>😁</Card>
+            <Card>😆</Card>
+            <Card>😅</Card>
         </div>
     )
+}
+
+const style = {
+    backgroundColor: '#464646ff',
+    flexGrow: 1,
+    display: 'grid',
+    gridTemplateColumns: 'repeat(5, 1fr)',
+    gridTemplateRows: 'repeat(5, 1fr)',
+    gap: '1em',
+    padding: '1em'
 }
