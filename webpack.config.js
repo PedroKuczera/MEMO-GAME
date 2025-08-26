7aa22411828ec154d2adec06403aec159360b5f7
Pedro Kuczera <pedrohkd001@gmail.com>
2025-08-26 08:50:07 -0300

    webpack-

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('node:path')
 module.exports = {
-    entry : './src/index.jsx',
+    entry : './src/index.js',
     output: {
         path: path.resolve(__dirname, './dist'),
         filename: 'bundle.js'
